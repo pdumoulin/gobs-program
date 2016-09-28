a54f218c97768ab3fffa8477bb64bb9f02010972
Paul Dumoulin <paul.l.dumoulin@gmail.com>
2016-09-28 02:28:24 +0000

    I don't know what you expected

---
+++ program.py
@@ -0,0 +1,24 @@
+
+# https://www.youtube.com/watch?v=JbnjusltDHk
+import sys
+
+def main():
+    print "Gob's Program:  Y/N?"
+    user_input = raw_input("?")
+    if user_input.lower() == 'y':
+        while True:
+            sys.stdout.write('Penus ')
+    elif user_input.lower() == 'n':
+        exit()
+    else:
+        print chr(27) + "[2J"
+        main()
+
+if __name__ == '__main__':
+    sys.stdout.write('\033[92m')
+    try:
+        main()
+    except:
+        pass
+    sys.stdout.write('\033[0m')
+
